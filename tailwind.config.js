f8ef037284f5462a44b14bc562cce2c6e93d54fc
behzad <behzadhammett66@gmail.com>
2026-07-08 04:35:40 +0330

    create some components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,38 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-
-    dark: 'class',
+    dark: "class",
 
     content: [
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-        './app/Livewire/**/*.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./app/Livewire/**/*.php",
     ],
 
     theme: {
         extend: {
-
             fontFamily: {
-                sans: ['Vazirmatn', ...defaultTheme.fontFamily.sans],
+                sans: ["Vazirmatn", ...defaultTheme.fontFamily.sans],
             },
             colors: {
-                bg: 'rgb(var(--color-bg) / <alpha-value>)',
-                surface: 'rgb(var(--color-surface) / <alpha-value>)',
-                border: 'rgb(var(--color-border) / <alpha-value>)',
-                text: 'rgb(var(--color-text) / <alpha-value>)',
-                muted: 'rgb(var(--color-text-muted) / <alpha-value>)',
-                primary: 'rgb(var(--color-primary) / <alpha-value>)',
+                bg: "rgb(var(--color-bg) / <alpha-value>)",
+                surface: "rgb(var(--color-surface) / <alpha-value>)",
+                border: "rgb(var(--color-border) / <alpha-value>)",
+                text: "rgb(var(--color-text) / <alpha-value>)",
+                muted: "rgb(var(--color-text-muted) / <alpha-value>)",
+                primary: "rgb(var(--color-primary) / <alpha-value>)",
             },
 
             boxShadow: {
-                'custome-shadow': '0 0 5px white',
+                "custome-shadow": "0 0 5px white",
             },
 
             btn: {
-                'global__btn': {
-                  
-              }  
+                global__btn: {},
             },
         },
     },
 
     plugins: [],
-}
+};
